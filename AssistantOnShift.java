1fd4177bff2655de03a23e5891a1f6a990755131
Matthew Cheng <38536795+matthewcheng222@users.noreply.github.com>
2021-02-23 21:12:37 +0000

    Update AssistantOnShift.java

--- AssistantOnShift.java
+++ AssistantOnShift.java
@@ -1,6 +1,36 @@
-import java.util.ArrayList;
-
 public class AssistantOnShift {
-    private ArrayList<Assistant> assistants;
+    private Assistant assistants;
     private String status;
+    private int seqID;
+    private String timeSlot;
+
+    public AssistantOnShift(Assistant assistants, String timeSlot) {
+        this.assistants = assistants;
+        this.timeSlot = timeSlot;
+        this.status = "FREE";
+    }
+
+    public void setStatusFree(String status) {
+        this.status = "FREE";
+    }
+
+    public void setStatusBusy(String status) {
+        this.status = "BUSY";
+    }
+
+    public void setSeqID(int seqID) {
+        this.seqID = seqID;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getSeqID() {
+        return seqID;
+    }
+
+    public String getAssistantOnShift() {
+        return "| " + timeSlot + " | " + status + " | " + assistants.getUniversityEmail() + " |";
+    }
 }
