4fb7c1787ca0b4f4700c65da330de29fd3b3516c
Matthew Cheng <38536795+matthewcheng222@users.noreply.github.com>
2021-02-24 22:46:18 +0000

    Update Booking.java

--- Booking.java
+++ Booking.java
@@ -1,10 +1,48 @@
-import java.util.ArrayList;
-
 public class Booking {
-    private ArrayList<BookableRoom> bookableRooms;
-    private ArrayList<AssistantOnShift> assistantOnShift;
+    private BookableRoom bookableRooms;
+    private AssistantOnShift assistantOnShifts;
+    private String status;
+    private String studentEmail;
+    private String timeSlot;
+    private int seqID;
 
-    public String getBooking() {
-        return "| " + "<dd/mm/yyyy HH:MM>" + " | " + bookableRooms.getStatus() + " | " + AssistantOnShift.getAssistants.getUniversityEmail() + " | " + bookableRooms.getRoom().getCode() + " | " + "<student_email>" + " |";
+    public Booking(String timeSlot, String studentEmail, BookableRoom bookableRooms, AssistantOnShift assistantOnShifts) {
+        this.timeSlot = timeSlot;
+        this.studentEmail = studentEmail;
+        this.bookableRooms = bookableRooms;
+        this.assistantOnShifts = assistantOnShifts;
+        this.status = "SCHEDULED";
+    }
+
+    public void setStatusScheduled() {
+        this.status = "SCHEDULED";
+    }
+
+    public void setStatusCompleted() {
+        this.status = "COMPLETED";
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setSeqID(int seqID) {
+        this.seqID = seqID;
+    }
+
+    public int getSeqID() {
+        return seqID;
+    }
+
+    public BookableRoom getBookableRooms() {
+        return bookableRooms;
+    }
+
+    public AssistantOnShift getAssistantOnShifts() {
+        return assistantOnShifts;
+    }
+
+    public String getBookings() {
+        return "| " + timeSlot + " | " + status + " | " + assistantOnShifts.getAssistants().getUniversityEmail() + " | " + bookableRooms.getRooms().getCode() + " | " + studentEmail + " |";
     }
 }
