96c6763a4ed77a27f6b500be9669db5ee16b1b28
Matthew Cheng <38536795+matthewcheng222@users.noreply.github.com>
2021-02-24 22:45:48 +0000

    Update AssistantOnShift.java

--- AssistantOnShift.java
+++ AssistantOnShift.java
@@ -1,35 +1,70 @@
 public class AssistantOnShift {
+    // private instance variables
     private Assistant assistants;
     private String status;
     private int seqID;
     private String timeSlot;
 
+    /**
+     * constructor for initialising assistant on shift
+     * 
+     * @param assistants the assistant to be initialised 
+     * @param timeSlot the time slot to be initialised
+     */
     public AssistantOnShift(Assistant assistants, String timeSlot) {
         this.assistants = assistants;
         this.timeSlot = timeSlot;
+        // setting the status of assistant on shift to be FREE when created
         this.status = "FREE";
     }
 
-    public void setStatusFree(String status) {
-        this.status = "FREE";
-    }
+    // public getters and setters for private instances 
 
-    public void setStatusBusy(String status) {
-        this.status = "BUSY";
-    }
-
-    public void setSeqID(int seqID) {
-        this.seqID = seqID;
-    }
-
+    /**
+     * @param status the status of assistant on shift to be set
+     */
     public void setStatus(String status) {
         this.status = status;
     }
 
+    /**
+     * @param seqID the sequential ID of assistant on shift to be set
+     */
+    public void setSeqID(int seqID) {
+        this.seqID = seqID;
+    }
+
+    /**
+     * @return the status of assistant on shift
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @return the sequential ID of assistant on shift
+     */
     public int getSeqID() {
         return seqID;
     }
 
+    /**
+     * @return the assistant of assistant on shift
+     */
+    public Assistant getAssistants() {
+        return assistants;
+    }
+
+    /**
+     * @return the time slot of assistant on shift
+     */
+    public String getTimeSlot() {
+        return timeSlot;
+    }
+
+    /**
+     * @return the print template of assistant on shift
+     */
     public String getAssistantOnShift() {
         return "| " + timeSlot + " | " + status + " | " + assistants.getUniversityEmail() + " |";
     }
