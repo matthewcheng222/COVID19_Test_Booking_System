14a705a7c3c375674c3c30c0f36bab02127b8eb7
Matthew Cheng <38536795+matthewcheng222@users.noreply.github.com>
2021-02-24 22:46:00 +0000

    Update BookableRoom.java

--- BookableRoom.java
+++ BookableRoom.java
@@ -1,45 +1,85 @@
 public class BookableRoom {
+    // private instance variables
     private Room rooms;
     private String timeSlot;
     private String status;
     private int occupancy;
     private int seqID;
 
+    /**
+     * constructor for initialising bookable rooms
+     * 
+     * @param rooms the room to be initialised
+     * @param timeSlot the time slot to be initialised
+     */
     public BookableRoom(Room rooms, String timeSlot) {
         this.rooms = rooms;
         this.timeSlot = timeSlot;
+        // setting the status of the bookable room to be EMPTY when created
         this.status = "EMPTY";
     }
 
-    public void setOccupancy(int occupancy) {
+    // public setters and getters for private instances 
+
+    /**
+     * setting the status of a bookable room with a given occupancy
+     * 
+     * @param occupancy the occupancy of the bookable room
+     */
+    public void setStatus(int occupancy) {
         this.occupancy = occupancy;
+        // setting status to EMPTY if occupancy is 0
         if (occupancy == 0) {
             status = "EMPTY";
         }
+        // setting status to AVAILABLE if occupancy is less than the room capacity
         else if (occupancy < rooms.getCapacity() && occupancy > 0) {
             status = "AVAILABLE";
         }
+        // setting status to FULL if occupancy is equal to the room capacity
         else if (occupancy == rooms.getCapacity() && occupancy > 0) {
             status = "FULL";
         }
     }
 
+    /**
+     * @param seqID the sequential ID of a bookable room
+     */
     public void setSeqID(int seqID) {
         this.seqID = seqID;
     }
 
+    /**
+     * @return the status of a bookable room
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * @return the room of a bookable room
+     */
     public Room getRooms() {
         return rooms;
     }
 
+    /**
+     * @return the sequential ID of a bookable room
+     */
     public int getSeqID() {
         return seqID;
     }
 
+    /**
+     * @return the time slot of a bookable room
+     */
+    public String getTimeSlot() {
+        return timeSlot;
+    }
+
+    /**
+     * @return the print template of a bookable room
+     */
     public String getBookableRooms() {
         return "| " + timeSlot + " | " + status + " | " + rooms.getCode() + " | occupancy: " + occupancy + " |";
     }
